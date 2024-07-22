9e2936abf3844fb9ab97507cc29e55fa591fce48
pranaybhosale1124 <pranaybhosale1124@gmail.com>
2024-07-22 14:37:18 +0530

    api-endpoint changed

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,7 +1,7 @@
 // environment.ts
 export const environment = {
     production: true,
-    apiUrl: 'https://chat-api-pranay-bhosales-projects.vercel.app',
+    apiUrl: 'https://pranay-chat-app-99290da8c080.herokuapp.com/',
     google_client_id:'812383719002-m9ng6v57jds0cd452f15nlq7lomivtca.apps.googleusercontent.com',
   };
   
