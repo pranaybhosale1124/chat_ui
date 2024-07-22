214d99371849cd5c9717e515c992e9916234a94d
pranaybhosale1124 <pranaybhosale1124@gmail.com>
2024-07-22 14:53:36 +0530

    api-endpoint changed

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,7 +1,7 @@
 // environment.ts
 export const environment = {
     production: true,
-    apiUrl: 'https://pranay-chat-app-99290da8c080.herokuapp.com/',
+    apiUrl: 'https://pranay-chat-app-99290da8c080.herokuapp.com',
     google_client_id:'812383719002-m9ng6v57jds0cd452f15nlq7lomivtca.apps.googleusercontent.com',
   };
   
